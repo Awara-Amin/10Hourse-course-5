d7aacdcb2366639644d6409a37f20d43e3459db3
AaA <67450869+Awara-Amin@users.noreply.github.com>
2021-05-17 18:31:40 +0300

    aa

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
-  // div or section or article all are alright Lovee
-  return (
-    <div>
-      <Person />
-      <Message />
-      <Awara />
-      {/* <h1>Hi babay</h1> */}
-      {/* <h2>Hi babay</h2> */}
-    </div>
-  );
+function BookList() {
+  return <section>
+    this is a booklist
+  </section>;
 }
 
-// inline function
-const Person = () => <h1>Hi babay</h1>;
-const Message = () => {
-  return <p>Hi babay</p>;
-};
-
-function Awara() {
-  return <h1> Awara, this should work too</h1>;
-}
-
-ReactDom.render(<Greeting />, document.getElementById("root"));
+ReactDom.render(<BookList />, document.getElementById("root"));
